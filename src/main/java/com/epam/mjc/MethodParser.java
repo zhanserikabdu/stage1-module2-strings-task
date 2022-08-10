210e5d5476755a443ea9ac66636e1b2915076f36
Alexander_Kuzyakov <alexander.kuzyakov@gmail.com>
2022-08-10 20:52:14 +0300

    initial commit

---
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -0,0 +1,25 @@
+package com.epam.mjc;
+
+public class MethodParser {
+
+    /**
+     * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
+     * signatureString is a java-like method signature with following parts:
+     *      1. access modifier - optional, followed by space: ' '
+     *      2. return type - followed by space: ' '
+     *      3. method name
+     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * Each argument consists of argument type and argument name, separated by space: ' '.
+     * Examples:
+     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     *      private void log(String value)
+     *      Vector3 distort(int x, int y, int z, float magnitude)
+     *      public DateTime getCurrentDateTime()
+     *
+     * @param signatureString source string to parse
+     * @return {@link MethodSignature} object filled with parsed values from source string
+     */
+    public MethodSignature parseFunction(String signatureString) {
+        throw new UnsupportedOperationException("You should implement this method.");
+    }
+}
---
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -0,0 +1,18 @@
+package com.epam.mjc;
+
+import java.util.Collection;
+import java.util.List;
+
+public class StringSplitter {
+
+    /**
+     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     *
+     * @param source source string
+     * @param delimiters collection of delimiter strings
+     * @return List of substrings
+     */
+    public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
+        throw new UnsupportedOperationException("You should implement this method.");
+    }
+}
